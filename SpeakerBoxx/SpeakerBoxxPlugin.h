6873aac17e1bba569451e781010125f820ec8610
Jean-Pierre Mouilleseaux <jpm@chordedconstructions.com>
2011-06-22 17:08:26 -0700

    initial project skeleton

---
+++ SpeakerBoxx/SpeakerBoxxPlugin.h
@@ -0,0 +1,20 @@
+//
+//  SpeakerBoxxPlugin.h
+//  SpeakerBoxx
+//
+//  Created by Jean-Pierre Mouilleseaux on 6/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface SpeakerBoxxPlugIn : QCPlugIn
+
+/*
+Declare here the properties to be used as input and output ports for the plug-in e.g.
+@property double inputFoo;
+@property (copy) NSString* outputBar;
+You can access their values in the appropriate plug-in methods using self.inputFoo or self.inputBar
+*/
+
+@end
